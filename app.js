4794544f2245194d6605812ef6d450564ad78238
David Stockdale <dstock3@gmail.com>
2022-05-02 21:52:52 -0500

    Added dotenv to app and replaced connection string with environment variable.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -12,7 +13,7 @@ const app = express();
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const dev_db_url = 'mongodb+srv://dstock:ihKNrn49hmsyuK41@cluster0.k9yut.mongodb.net/grocery_inventory?retryWrites=true&w=majority';
+const dev_db_url = process.env.MONGO_CONNECT;
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 const db = mongoose.connection;
